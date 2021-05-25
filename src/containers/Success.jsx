955a93396c7ac653eeff8788d4b8312569165d7f
Andres Norato <carnorat@gmail.com>
2021-05-25 10:47:16 -0500

    Se agrego la api de google

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
 import '../styles/components/Success.css';
 
 const Success = () => {
+
+    const {state} = useContext(AppContext);
+    const {buyer} = state;
+
     return (
         <div className="Success">
             <div className="Success-content">
-                <h2>Oscar, Gracias por tu compra</h2>
+                <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
                 <span>Tu pedido llegara en 3 dias a tu direccion</span>
                 <div className="Success-map">
-                    Google-map
+                 <Map />   
                 </div>
             </div>
         </div>
@@ -16,3 +22,6 @@ const Success = () => {
 }
 
 export default Success;
+
+
+//  
